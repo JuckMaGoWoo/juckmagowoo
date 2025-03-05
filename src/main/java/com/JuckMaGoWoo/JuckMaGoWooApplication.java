5746745e9716e8eab7e8ab85491edb19c545f90a
이민구 <goorm@goormui-MacBookPro.local>
2025-03-05 17:56:50 +0900

    [chore]: initial commit

---
+++ src/main/java/com/JuckMaGoWoo/JuckMaGoWooApplication.java
@@ -0,0 +1,13 @@
+package com.JuckMaGoWoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuckMaGoWooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuckMaGoWooApplication.class, args);
+    }
+
+}
