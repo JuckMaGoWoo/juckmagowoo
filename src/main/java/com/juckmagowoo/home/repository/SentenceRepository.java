af7823857bcb6e3dbcd23ea42c8224ca18712476
TAEHYEON <disney1026dlxogus@gmail.com>
2025-03-07 05:46:29 +0900

    잘가라

--- src/main/java/com/juckmagowoo/home/repository/SentenceRepository.java
+++ src/main/java/com/juckmagowoo/home/repository/SentenceRepository.java
@@ -2,14 +2,26 @@ package com.juckmagowoo.home.repository;
 
 import com.juckmagowoo.home.entity.Sentence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SentenceRepository extends JpaRepository<Sentence, Long> {
-    // 우울 척도 또는 언어 능력이 70 이상인 문장 조회
-    List<Sentence> findByUser_UserIdAndAnxietyScoreGreaterThanEqualOrLogicalScoreLessThanEqual(Long userId, Long anxietyScore, Long logicalScore);
+
+//    // 우울 척도 또는 언어 능력이 70 이상인 문장 조회
+//    List<Sentence> findByUser_UserIdAndAnxietyScoreGreaterThanEqualOrLogicalScoreLessThanEqual(Long userId, Long anxietyScore, Long logicalScore);
+
+    @Query("SELECT s FROM Sentence s " +
+            "WHERE s.user.userId = :userId " +
+            "AND (s.anxietyScore >= :anxietyScore OR s.logicalScore <= :logicalScore)")
+    List<Sentence> findSentencesByUserIdAndAnxietyOrLogical(
+            @Param("userId") Long userId,
+            @Param("anxietyScore") Long anxietyScore,
+            @Param("logicalScore") Long logicalScore
+    );
 
 
     List<Sentence> findByUser_UserId(Long userId);
